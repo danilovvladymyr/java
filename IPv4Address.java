5b6c4ce3be49e79489c44369fc329987b4de979a
devel <vladymyr.danilov@gmail.com>
2015-07-21 02:35:04 +0300

    Create repo c&c/java

---
+++ IPv4Address.java
@@ -0,0 +1,13 @@
+public class IPv4Address {
+    public IPv4Address(String address) throws IllegalArgumentException {}
+    public IPv4Address(long address) throws IllegalArgumentException {}
+    
+    public boolean lessThan(IPv4Address address) {}
+    public boolean greatThan(IPv4Address address) {}
+    public boolean equals(IPv4Address address) {}
+    
+    public String toString() {}
+    public long toLong() {
+        
+    }
+}
